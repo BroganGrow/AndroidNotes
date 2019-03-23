43fcbc6b3943e85932e4bc649ca027108381da8e
Brain-RD <1057234389@qq.com>
2019-03-23 18:31:36 +0800

    添加

---
+++ app/src/main/java/com/brainbg/learngraphics/act/MainActivity.java
@@ -0,0 +1,39 @@
+package com.brainbg.learngraphics.act;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.brainbg.learngraphics.R;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_paint).setOnClickListener(this);
+        findViewById(R.id.btn_canvas).setOnClickListener(this);
+        findViewById(R.id.btn_path).setOnClickListener(this);
+        findViewById(R.id.btn_rect).setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_paint:
+                startActivity(new Intent(MainActivity.this, PaintActivity.class));
+                break;
+            case R.id.btn_canvas:
+                break;
+            case R.id.btn_path:
+                break;
+            case R.id.btn_rect:
+                break;
+        }
+
+    }
+}
---
+++ app/src/main/java/com/brainbg/learngraphics/act/PaintActivity.java
@@ -0,0 +1,15 @@
+package com.brainbg.learngraphics.act;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.brainbg.learngraphics.R;
+
+public class PaintActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paint);
+    }
+}
