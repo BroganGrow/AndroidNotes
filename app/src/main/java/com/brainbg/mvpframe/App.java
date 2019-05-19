561c37ae7f577fd5a17671eb0879add15e8c441a
Brainbg <1057234389@qq.com>
2019-05-19 13:18:37 +0800

    创建项目

---
+++ app/src/main/java/com/brainbg/mvpframe/App.java
@@ -0,0 +1,20 @@
+package com.brainbg.mvpframe;
+
+import android.app.Application;
+
+/**
+ * <pre>
+ *     author : Brainbg
+ *     e-mail : xxx@xx
+ *     time   : 2019/05/19
+ *     desc   :
+ * </pre>
+ */
+public class App extends Application {
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
