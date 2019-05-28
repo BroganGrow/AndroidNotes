4ceca79e5714bc781a73b73e297c432782be62b1
Brainbg <1057234389@qq.com>
2019-05-28 21:02:30 +0800

    mvp

---
+++ app/src/main/java/com/brainbg/mvpframe/base/BaseFragment.java
@@ -0,0 +1,70 @@
+package com.brainbg.mvpframe.base;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public abstract class BaseFragment extends Fragment implements BaseView {
+
+    public abstract int getContentViewId();
+
+    protected abstract void initAllMembersView(Bundle savedInstanceState);
+
+    protected Context mContext;
+    protected View mRootView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mRootView = inflater.inflate(getContentViewId(), container, false);
+        this.mContext = getActivity();
+        initAllMembersView(savedInstanceState);
+        return mRootView;
+    }
+
+    @Override
+    public void showLoading() {
+        checkActivityAttached();
+        ((BaseActivity) mContext).showLoading();
+    }
+
+    @Override
+    public void hideLoading() {
+        checkActivityAttached();
+        ((BaseActivity) mContext).hideLoading();
+    }
+
+    @Override
+    public void showToast(String msg) {
+        checkActivityAttached();
+        ((BaseActivity) mContext).showToast(msg);
+    }
+
+    @Override
+    public void showErr() {
+        checkActivityAttached();
+        ((BaseActivity) mContext).showErr();
+    }
+
+    protected boolean isAttachedContext() {
+        return getActivity() != null;
+    }
+
+    /**
+     * 检查activity连接情况
+     */
+    public void checkActivityAttached() {
+        if (getActivity() == null) {
+            throw new ActivityNotAttachedException();
+        }
+    }
+
+    public static class ActivityNotAttachedException extends RuntimeException {
+        public ActivityNotAttachedException() {
+            super("Fragment has disconnected from Activity ! - -.");
+        }
+    }
+
+}
---
+++ app/src/main/java/com/brainbg/mvpframe/model/MvpModel.java
@@ -0,0 +1,39 @@
+package com.brainbg.mvpframe.model;
+
+import android.os.Handler;
+
+import com.brainbg.mvpframe.callback.MvpCallback;
+
+public class MvpModel {
+
+    /**
+     * 获取网络接口数据
+     *  @param param    请求参数
+     * @param callback 数据回调接口
+     */
+    public static void getNetData(final String param, final MvpCallback callback) {
+
+        // 利用postDelayed方法模拟网络请求数据的耗时操作
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                switch (param) {
+                    case "normal":
+                        callback.onSuccess("根据参数" + param + "的请求网络数据成功");
+                        break;
+
+                    case "failure":
+                        callback.onFailure("请求失败：参数有误");
+                        break;
+
+                    case "error":
+                        callback.onError();
+                        break;
+                }
+                callback.onComplete();
+            }
+
+        }, 2000);
+    }
+
+}
