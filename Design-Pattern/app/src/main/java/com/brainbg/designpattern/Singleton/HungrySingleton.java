097781ffca9e15e72eede19739b802bc199ae6f1
黄碧冠 <brainbg@huangbiguandeMacBook-Pro.local>
2019-08-02 08:33:53 +0800

    改修

--- Design-Pattern/app/src/main/java/com/brainbg/designpattern/Singleton/HungrySingleton.java
+++ Design-Pattern/app/src/main/java/com/brainbg/designpattern/Singleton/HungrySingleton.java
@@ -9,11 +9,15 @@ package com.brainbg.designpattern.Singleton;
  * </pre>
  */
 public class HungrySingleton {
+    // 1. 加载该类时，单例就会自动被创建
     private static final HungrySingleton instance = new HungrySingleton();
 
+    // 2. 构造函数 设置为 私有权限
+    // 原因：禁止他人创建实例
     private HungrySingleton() {
     }
 
+    // 3. 通过调用静态方法获得创建的单例
     public static HungrySingleton getInstance() {
         return instance;
     }
